52d3154a96cc56c59133c3f56d082d0f79572c7a
Emil Laine <emil@cs.helsinki.fi>
2018-10-13 17:37:26 +0300

    Configure production build for Heroku

--- src-backend/database.ts
+++ src-backend/database.ts
@@ -5,10 +5,11 @@ const isDevelopment = process.env.NODE_ENV !== 'production'
 
 const options: ConnectionOptions = {
   type: 'postgres',
+  url: process.env.DATABASE_URL,
   synchronize: isDevelopment,
-  entities: ['src-common/entity/**/*.ts'],
-  migrations: ['src-backend/migration/**/*.ts'],
-  subscribers: ['src-backend/subscriber/**/*.ts'],
+  entities: [isDevelopment ? 'src-common/entity/**/*.ts' : 'build-backend/src-common/entity/**/*.js'],
+  migrations: [isDevelopment ? 'src-backend/migration/**/*.ts' : 'build-backend/src-common/migration/**/*.js'],
+  subscribers: [isDevelopment ? 'src-backend/subscriber/**/*.ts' : 'build-backend/src-common/subscriber/**/*.js'],
   cli: {
     entitiesDir: 'src-common/entity',
     migrationsDir: 'src-backend/migration',
--- src-backend/server.ts
+++ src-backend/server.ts
@@ -1,5 +1,6 @@
 import 'reflect-metadata'
 import 'express-async-errors'
+import * as path from 'path'
 import * as express from 'express'
 import * as bodyParser from 'body-parser'
 import tasks from './routes/tasks'
@@ -8,7 +9,17 @@ import { connectToDatabase } from './database'
 connectToDatabase().then(() => {
   const app = express()
   app.use(bodyParser.json())
+
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('build'))
+
+    app.get('/', (req, res) => {
+      res.sendFile(path.join('build', 'index.html'))
+    })
+  }
+
   app.use(tasks)
+
   app.listen(process.env.PORT || 3001)
 }).catch(error => {
   console.log(error)
