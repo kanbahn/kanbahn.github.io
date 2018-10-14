9cd0faa099f0c99f301d2b3788f4f663be092325
Emil Laine <emil@cs.helsinki.fi>
2018-10-14 15:30:26 +0300

    Make redux actions type-safe

--- src/reducers/taskReducer.ts
+++ src/reducers/taskReducer.ts
@@ -3,6 +3,30 @@ import { Dispatch, Reducer } from 'redux'
 import { Task } from '../../src-common/entity/Task'
 import { debouncedPatchJSON, patchJSON, postJSON } from '../fetch'
 
+interface NewTask {
+  type: 'NEW-TASK'
+  newTask: Task
+}
+
+interface EditTask {
+  type: 'EDIT-TASK'
+  id: number
+  title: string
+}
+
+interface MoveTask {
+  type: 'MOVE-TASK'
+  id: number
+  column: string
+}
+
+interface ReceiveTasks {
+  type: 'RECEIVE-TASKS'
+  tasks: Task[]
+}
+
+type TaskAction = NewTask | EditTask | MoveTask | ReceiveTasks
+
 export interface StoreState {
   tasks: Task[]
 }
@@ -11,22 +35,26 @@ const initialState: StoreState = {
   tasks: []
 }
 
-const taskReducer: Reducer<StoreState> = (state = initialState, action) => {
+const taskReducer: Reducer<StoreState, TaskAction> = (state = initialState, action) => {
   switch (action.type) {
     case 'NEW-TASK':
       return { ...state, tasks: state.tasks.concat(action.newTask) }
+
     case 'EDIT-TASK':
       const taskToEdit = state.tasks.find(task => task.id === action.id)
       if (!taskToEdit) return state
       const editedTask: Task = { ...taskToEdit, title: action.title }
       return { ...state, tasks: state.tasks.map(task => task.id !== action.id ? task : editedTask) }
+
     case 'MOVE-TASK':
       const taskToMove = state.tasks.find(task => task.id === action.id)
       if (!taskToMove) return state
       const movedTask: Task = { ...taskToMove, column: action.column }
       return { ...state, tasks: state.tasks.map(task => task.id !== action.id ? task : movedTask) }
+
     case 'RECEIVE-TASKS':
       return { ...state, tasks: action.tasks }
+
     default:
       return state
   }
@@ -37,7 +65,7 @@ export const taskCreation = (laneName: string, columnName: string) => {
   columnName = columnName.toLowerCase()
   const newTaskObject: Omit<Task, 'id'> = { title: 'empty task', lane: laneName, column: columnName }
 
-  return async (dispatch: Dispatch) => {
+  return async (dispatch: Dispatch<TaskAction>) => {
     const newTask: Task = await postJSON('/api/tasks', newTaskObject)
     return dispatch({
       type: 'NEW-TASK',
@@ -49,7 +77,7 @@ export const taskCreation = (laneName: string, columnName: string) => {
 export const taskEdit = (taskObj: { id: number, title: string }) => {
   const { id, title } = taskObj
 
-  return async (dispatch: Dispatch) => {
+  return async (dispatch: Dispatch<TaskAction>) => {
     await debouncedPatchJSON(`/api/tasks/${id}`, { title })
     return dispatch({
       type: 'EDIT-TASK',
@@ -60,7 +88,7 @@ export const taskEdit = (taskObj: { id: number, title: string }) => {
 }
 
 export const moveTask = (taskId: number, column: string) => {
-  return async (dispatch: Dispatch) => {
+  return async (dispatch: Dispatch<TaskAction>) => {
     await patchJSON(`/api/tasks/${taskId}`, { column })
     return dispatch({
       type: 'MOVE-TASK',
@@ -70,7 +98,7 @@ export const moveTask = (taskId: number, column: string) => {
   }
 }
 
-export const receiveTasks = (tasks: Task[]) => {
+export const receiveTasks = (tasks: Task[]): TaskAction => {
   return {
     type: 'RECEIVE-TASKS',
     tasks
